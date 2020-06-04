f69e83703ec0d6bd399c951f35bf7cee6244d4ea
Muneeer <muneerabbas50@gmail.com>
2020-06-04 21:33:39 +0900

    ProfileUI Updatedddd

--- app/src/main/java/com/fas/smash_k/SettingsActivity.java
+++ app/src/main/java/com/fas/smash_k/SettingsActivity.java
@@ -1,15 +1,153 @@
 package com.fas.smash_k;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class SettingsActivity extends Activity {
+import java.io.IOException;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class SettingsActivity extends Activity implements View.OnClickListener {
+
+    //Changeeed
+    private static final int PICK_IMAGE = 1;
+
+    Dialog MyDialoge;
+    TextView changeName,changeUsername,changeBirthday;
+    Uri imageUri;
+    private CircleImageView ProfileImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings);
 
 
+        changeName=findViewById(R.id.changeName);
+        changeUsername=findViewById(R.id.changeUsername);
+        changeBirthday=findViewById(R.id.changeBirthday);
+        ProfileImage=findViewById(R.id.profileimage);
+        ProfileImage.setOnClickListener(this);
+
+        changeName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MyDialoge=new Dialog(SettingsActivity.this);
+                MyDialoge.setContentView(R.layout.changename);
+                MyDialoge.setTitle("Change Name");
+
+
+                final EditText getName=(EditText)MyDialoge.findViewById(R.id.getName);
+                Button done=(Button)MyDialoge.findViewById(R.id.OK);
+                MyDialoge.show();
+
+                done.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        changeName.setText(getName.getText().toString());
+
+                        MyDialoge.cancel();
+
+                    }
+                });
+            }
+        });
+
+
+        changeUsername.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MyDialoge=new Dialog(SettingsActivity.this);
+                MyDialoge.setContentView(R.layout.changeusername);
+                MyDialoge.setTitle("Change Name");
+
+
+                final EditText getName=(EditText)MyDialoge.findViewById(R.id.getName);
+                Button done=(Button)MyDialoge.findViewById(R.id.OK);
+                MyDialoge.show();
+
+                done.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        changeUsername.setText(getName.getText().toString());
+
+                        MyDialoge.cancel();
+
+                    }
+                });
+
+            }
+        });
+
+
+        changeBirthday.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                MyDialoge=new Dialog(SettingsActivity.this);
+                MyDialoge.setContentView(R.layout.changebirthday);
+                MyDialoge.setTitle("Change Name");
+
+
+                final EditText getName=(EditText)MyDialoge.findViewById(R.id.getName);
+                Button done=(Button)MyDialoge.findViewById(R.id.OK);
+                MyDialoge.show();
+
+                done.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        changeBirthday.setText(getName.getText().toString());
+
+                        MyDialoge.cancel();
+
+                    }
+                });
+
+            }
+        });
+
+
+
+        // Hide status bar
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+// Show status bar
+//        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE && resultCode == RESULT_OK && data != null) {
+            imageUri = data.getData();
+
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), imageUri);
+                ProfileImage.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        Intent gallery = new Intent();
+        gallery.setType("image/*");
+        gallery.setAction(Intent.ACTION_GET_CONTENT);
+
+        startActivityForResult(Intent.createChooser(gallery, "Select Picture"), PICK_IMAGE);
+
+    }
+
+    //Changedddd
 }
--- app/src/main/java/com/fas/smash_k/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/fas/smash_k/ui/notifications/NotificationsFragment.java
@@ -2,13 +2,18 @@ package com.fas.smash_k.ui.notifications;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -24,13 +29,19 @@ import com.fas.smash_k.R;
 import com.fas.smash_k.SettingsActivity;
 import com.google.android.material.appbar.CollapsingToolbarLayout;
 
-public class NotificationsFragment extends Fragment {
+import java.io.IOException;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+import static android.app.Activity.RESULT_OK;
+
+public class NotificationsFragment extends Fragment implements View.OnClickListener {
 
     private NotificationsViewModel notificationsViewModel;
     Button settingsButton;
-    Button profilePictureBtn;
-    Button btnDickyMuneer;
+    Button ProfileImage;
     Toolbar toolbar;
+    Uri imageUri;
     CollapsingToolbarLayout ctoolbar;
     private  View root;
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -40,23 +51,47 @@ public class NotificationsFragment extends Fragment {
         root = inflater.inflate(R.layout.fragment_notifications, container, false);
 
         settingsButton = root.findViewById(R.id.settings);
-        profilePictureBtn=root.findViewById(R.id.uploadProfilePicture);
-        profilePictureBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getContext(),"Upload Clicked",Toast.LENGTH_LONG).show();
-            }
+        ProfileImage=root.findViewById(R.id.profileimage);
+        ProfileImage.setOnClickListener(this);
+
+
+        settingsButton.setOnClickListener(v -> {
+            Intent settingsIntent=new Intent(getActivity(), SettingsActivity.class);
+            startActivity(settingsIntent);
         });
-        settingsButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getContext(),"Settings Clicked",Toast.LENGTH_LONG).show();
-                Intent settingsIntent=new Intent(getActivity(), SettingsActivity.class);
-                startActivity(settingsIntent);
-            }
-        });
+
+
+
         return root;
+
     }
 
 
+    @Override
+    public void onClick(View view) {
+            Intent gallery = new Intent();
+            gallery.setType("image/*");
+            gallery.setAction(Intent.ACTION_GET_CONTENT);
+
+            startActivityForResult(Intent.createChooser(gallery, "Select Picture"), 1);
+        }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1 && resultCode == RESULT_OK && data != null) {
+            imageUri = data.getData();
+
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), imageUri);
+                ProfileImage.setBackground(new BitmapDrawable(getResources(),bitmap));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+    }
 }
